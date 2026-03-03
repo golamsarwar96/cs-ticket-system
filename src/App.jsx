53180383a6dff32ee1e70273efc0f5c2d4d53d61
MD. Golam Sarwar <sarwartasin1896@gmail.com>
2026-03-04 02:11:22 +0600

    Basic Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="max-w-300 mx-auto">
+      {" "}
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
