1d103ef1ce5a3da1b30e3d4b5de558f94bc79057
MD. Golam Sarwar <sarwartasin1896@gmail.com>
2026-03-05 01:14:29 +0600

    Made json, loaded data in main component and used props to pass data to customerTickets component and showed json data using use hook. Showed the data in 2 column layout using grid and made it mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { Suspense } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 import CustomerTickets from "./components/CustomerTickets";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
+const fetchTicketData = async () => {
+  const res = await fetch("data.json");
+  return res.json();
+};
+
 function App() {
+  const ticketPromise = fetchTicketData();
   return (
     <div className="max-w-300 mx-auto">
       {" "}
       <NavBar></NavBar>
       <Banner></Banner>
-      <CustomerTickets></CustomerTickets>
+      <Suspense
+        fallback={
+          <span className="loading loading-spinner text-primary"></span>
+        }
+      >
+        <CustomerTickets ticketPromise={ticketPromise}></CustomerTickets>
+      </Suspense>
       <Footer></Footer>
     </div>
   );
--- src/components/CustomerTickets.jsx
+++ src/components/CustomerTickets.jsx
@@ -1,8 +1,10 @@
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { use } from "react";
 
-const CustomerTickets = () => {
+const CustomerTickets = ({ ticketPromise }) => {
+  const ticketData = use(ticketPromise);
+  console.log(ticketData);
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold text-mist-700 text-center lg:text-left mb-3">
@@ -10,58 +12,37 @@ const CustomerTickets = () => {
       </h1>
       <div className="grid gap-4 lg:grid-cols-3 grid-cols-1">
         {/* tickets section */}
-        <div className="lg:flex gap-9 lg:col-span-2">
+        <div className="grid lg:grid-cols-2 gap-5 lg:col-span-2">
           {/* Single ticket */}
-          <div class="card lg:w-96 bg-base-100 card-sm shadow-lg">
-            <div class="card-body">
-              <div className="flex justify-between gap-1">
-                <h2 class="card-title flex-1">Small Card</h2>
-                <span className="bg-[#B9F8CF] font-bold px-4 py-1 rounded-2xl text-[#0B5E06]">
-                  <FontAwesomeIcon className="text-[#02A53B]" icon={faCircle} />{" "}
-                  Open
-                </span>
-              </div>
-              <p className="text-mist-500 text-medium">
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
-              </p>
-              <div class="card-actions flex justify-between">
-                <div className="flex gap-7">
-                  <p className="text-mist-400">#1001</p>
-                  <p className="text-orange-700">High Priority</p>
+          {ticketData.map((ticket) => (
+            <div className="card lg:w-96 bg-base-100 card-sm shadow-lg">
+              <div className="card-body">
+                <div className="flex justify-between gap-1">
+                  <h2 className="card-title flex-1">{ticket.title}</h2>
+                  <span className="bg-[#B9F8CF] font-bold px-3 py-1 rounded-2xl text-[#0B5E06]">
+                    <FontAwesomeIcon
+                      className="text-[#02A53B]"
+                      icon={faCircle}
+                    />{" "}
+                    {ticket.status}
+                  </span>
                 </div>
-                <div className="flex gap-5 text-mist-500">
-                  <p>John Smith</p>
-                  <p>1/15/2024</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card lg:w-96 bg-base-100 card-sm shadow-lg">
-            <div class="card-body">
-              <div className="flex justify-between gap-1">
-                <h2 class="card-title flex-1">Small Card</h2>
-                <span className="bg-[#B9F8CF] font-bold px-4 py-1 rounded-2xl text-[#0B5E06]">
-                  <FontAwesomeIcon className="text-[#02A53B]" icon={faCircle} />{" "}
-                  Open
-                </span>
-              </div>
-              <p className="text-mist-500 text-medium">
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
-              </p>
-              <div class="card-actions flex justify-between">
-                <div className="flex gap-7">
-                  <p className="text-mist-500">#1001</p>
-                  <p className="text-orange-700">High Priority</p>
-                </div>
-                <div className="flex gap-5 text-mist-500">
-                  <p>John Smith</p>
-                  <p>1/15/2024</p>
+                <p className="text-mist-500 text-medium">
+                  {ticket.description}
+                </p>
+                <div className="card-actions flex justify-between">
+                  <div className="flex gap-7">
+                    <p className="text-mist-400">{ticket.id}</p>
+                    <p className="text-orange-700">{ticket.priority}</p>
+                  </div>
+                  <div className="flex gap-5 text-mist-500">
+                    <p>{ticket.customer}</p>
+                    <p>{ticket.createdAt}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
         <div className="lg:col-span-1 space-y-4 p-2">
           {/* Task Section */}
